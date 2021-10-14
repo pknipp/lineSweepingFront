777fcd9360a11fba9a882f19b7a3f850d292838a
Peter Knipp <peteraknipp@gmail.com>
2021-10-14 12:09:19 -0400

    Redirect the fetch, from localhost to heroku.

--- src/App.js
+++ src/App.js
@@ -1,2 +1,17 @@
-const App = () => <div>This is App.js</div>
+import React, {useState, useEffect} from 'react';
+
+const App = () => {
+    const [message, setMessage] = useState('initial message');
+
+    useEffect(() => {
+        (async () => {
+            // let backURL = "http://127.0.0.1:5000";
+            let backURL = "https://line-sweeping-back.herokuapp.com";
+            const response = await fetch(backURL);
+            let data = await response.json();
+            setMessage(data.message);
+        })()
+    }, []);
+    return <div>The back-end says {message}.</div>
+}
 export default App;
